be3bf2ff70e5d462a0387c38dff7d7261c9e3843
MaximShtaba <111359641+MaximShtaba@users.noreply.github.com>
2023-02-13 16:55:23 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig){
+	let stack = [];
+	let brackets = {
+		")": "(",
+		"}": "{",
+		"]": "[",
+	}
+	for (let i = 0; i < str.length; i++) {
+		let curent = str[i];
+		if (isClosedBracket(curent)) {
+			if (brackets[curent] !== stack.pop()) return false;
+		} else {
+			stack.push(curent);
+		}
+	}
+	return stack.length === 0;
+}
+
+
+function isClosedBracket(ch) {
+	return ["}", ")", "]",].includes(ch) > -1
 }
