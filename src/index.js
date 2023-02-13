bd0d086e10f3c16589e551efb4cfac245c7b9796
MaximShtaba <111359641+MaximShtaba@users.noreply.github.com>
2023-02-13 23:58:15 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-module.exports = function check(str, bracketsConfig) {
 	if (str.length%2!==0) return false
+	
 	let arr = [];
 	for (let i = 0; i < str.length; i++) {
 	 for (let j = 0; j<bracketsConfig.length; j++){
+		if (str[0] === bracketsConfig[j][1]){ return false}
 		
-		if (str[i] === bracketsConfig[j][0]) { arr.push(str[i])}
+		else if (str[i] === bracketsConfig[j][0]) { arr.push(str[i])}
 		
 		else if (arr[arr.length-1] !== bracketsConfig[j][1] ) {arr.pop()}
 		
@@ -13,4 +14,3 @@ module.exports = function check(str, bracketsConfig) {
 
 	}
  if (arr.length === 0) {return true} else {return false }
-}
